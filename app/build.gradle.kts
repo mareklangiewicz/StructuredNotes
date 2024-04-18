6f8237824376eb0321b3203b0b2953f30e9819b2
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-04-18 14:24:05 +0200

    fun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,30 +1,38 @@
+@file:Suppress("DEPRECATION", "unused", "UnusedVariable")
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
+import pl.mareklangiewicz.defaults.*
+import pl.mareklangiewicz.deps.*
+import pl.mareklangiewicz.utils.*
+
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
-    kotlin("android")
+    plugAll(plugs.AndroAppNoVer, plugs.KotlinAndro, plugs.MavenPublish, plugs.Signing)
 }
 
-android {
-    compileSdk = Vers.androidCompileSdk
-    defaultConfig {
-        applicationId = "pl.mareklangiewicz.notes"
-        minSdk = Vers.androidMinSdk
-        targetSdk = Vers.androidTargetSdk
-        versionCode = 4
-        versionName = "0.04"
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-        useIR = true
-    }
-}
+defaultBuildTemplateForAndroApp()
+
+// android {
+//     compileSdk = Vers.androidCompileSdk
+//     defaultConfig {
+//         applicationId = "pl.mareklangiewicz.notes"
+//         minSdk = Vers.androidMinSdk
+//         targetSdk = Vers.androidTargetSdk
+//         versionCode = 4
+//         versionName = "0.04"
+//     }
+//     kotlinOptions {
+//         jvmTarget = "1.8"
+//         useIR = true
+//     }
+// }
 
 dependencies {
     implementation(project(":logic"))
     implementation(project(":widgets"))
-    implementation(Deps.androidxBrowser)
-    implementation(platform(Deps.firebaseAndroidBoM))
-    implementation(Deps.firebaseAnalyticsKtx)
-    implementation(Deps.firebaseCrashlyticsKtx)
+    // implementation(Deps.androidxBrowser)
+    // implementation(platform(Deps.firebaseAndroidBoM))
+    // implementation(Deps.firebaseAnalyticsKtx)
+    // implementation(Deps.firebaseCrashlyticsKtx)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,42 @@
-
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    includeBuild("../DepsKt")
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+
+  val depsDir = File(rootDir, "../DepsKt").normalize()
+  val depsInclude =
+    // depsDir.exists()
+    false
+  if (depsInclude) {
+    logger.warn("Including local build $depsDir")
+    includeBuild(depsDir)
+  }
 }
 
 plugins {
-    id("pl.mareklangiewicz.deps.settings") version "0.2.99"
-    // https://plugins.gradle.org/search?term=mareklangiewicz
-    id("com.gradle.enterprise") version "3.17.2"
-    // https://docs.gradle.com/enterprise/gradle-plugin/
+  id("pl.mareklangiewicz.deps.settings") version "0.2.99"
+  // https://plugins.gradle.org/search?term=mareklangiewicz
+  id("com.gradle.enterprise") version "3.17.2"
+  // https://docs.gradle.com/enterprise/gradle-plugin/
 }
 
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
-        publishOnFailure()
+develocity {
+  buildScan {
+    termsOfUseUrl = "https://gradle.com/terms-of-service"
+    termsOfUseAgree = "yes"
+    publishing.onlyIf { // careful with publishing fails especially from my machine (privacy)
+      true &&
+        it.buildResult.failures.isNotEmpty() &&
+        // it.buildResult.failures.isEmpty() &&
+        System.getenv("GITHUB_ACTIONS") == "true" &&
+        // System.getenv("GITHUB_ACTIONS") != "true" &&
+        true
+      // false
     }
+  }
 }
-
 
 include(":common")
 include(":logic")
